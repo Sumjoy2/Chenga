63f799f47bf502444b363bd4817cdd104542d6d6
Sumjoy2 <5011286mountjoy@gmail.com>
2023-06-12 13:44:16 -0700

    ExplodieMayhapsBasics

--- Assets/scrip/FunBloc.cs
+++ Assets/scrip/FunBloc.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FunBloc : MonoBehaviour
 {
     public GameObject[] AllBloks;
+    int randOutput;
     // Start is called before the first frame update
     void Start()
     {
